fbb50247d445f7068cfd71ae9780e24eb9ab6dd8
Assyl Chouikh <assylchk@gmail.com>
2024-06-28 00:35:09 +0100

    UpdateDeployConfigs

--- prisma/migrations/20240627151448_add_statut_feild/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Tetstimonials" ADD COLUMN     "statut" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240627233157_initial/migration.sql
+++ prisma/migrations/20240627233157_initial/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "Tetstimonials" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "message" TEXT NOT NULL,
+    "statut" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Tetstimonials_pkey" PRIMARY KEY ("id")
