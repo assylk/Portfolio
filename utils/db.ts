0e6eb76615df36ed475c5d55d69b9545c457640a
Assyl Chouikh <assylchk@gmail.com>
2024-06-28 00:02:56 +0100

    last commit

--- utils/db.ts
+++ utils/db.ts
@@ -1,9 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from '@prisma/client';
+
 
 const prisma = new PrismaClient();
 export default prisma;
-
-/* import { PrismaClient } from "@prisma/client";
+ */
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
@@ -18,4 +19,3 @@ const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
- */
